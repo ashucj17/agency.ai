eea454312ea214cee0b5746ac6b2527014c145c2
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-08-25 15:45:27 +0530

    Issue with default dark navbar and disabled toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Routing from './Routing/Routing'
 
 
 function App() {
 
-  const [theme, setTheme] = useState('light')
+const [theme, setTheme] = useState(() => {
+  // if not saved before, check system preference
+  const saved = localStorage.getItem("theme");
+  if (saved) return saved;
+
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  return prefersDark ? "dark" : "light";
+});
+
 
   return (
     <>
-     <div className='relative'> {/* {dark:bg-black} */}
+     <div className='dark:bg-black relative'>
      <Navbar theme={theme} setTheme={setTheme} />
      <main>
       <Routing/>
